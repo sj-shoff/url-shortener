f8ded22d0fb7ba41555c58c304e5ef5372a41cc7
sj-shoff <atryakhin0306@gmail.com>
2025-03-11 13:42:45 +0300

    redirect mocks, api, main

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -49,8 +49,20 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
-	router.Post("/url", save.New(log, storage))
+	// routes && server
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+
+		router.Post("/", save.New(log, storage))
+		//TODO: DELETE
+	})
+
 	router.Get("/{alias}", redirect.New(log, storage))
+
+	log.Info("starting server", slog.String("address", cfg.Address))
+
 	srv := &http.Server{
 		Addr:         cfg.Address,
 		Handler:      router,
@@ -58,8 +70,6 @@ func main() {
 		WriteTimeout: cfg.HTTPServer.Timeout,
 		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
 	}
-
-	log.Info("starting server", slog.String("address", cfg.Address))
 
 	if err := srv.ListenAndServe(); err != nil {
 		log.Error("failed to start server")
--- internal/http-server/handlers/redirect/redirect.go
+++ internal/http-server/handlers/redirect/redirect.go
@@ -18,7 +18,6 @@ import (
 // URLGetter is an interface for getting url by alias.
 //
 //go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name=URLGetter
-
 type URLGetter interface {
 	GetURL(alias string) (string, error)
 }
